c6caa03dc65e2b4974520c84e920350f358e67d1
Moiz Wilayat <moiz.wilayat@student.howest.be>
2026-02-16 21:14:15 +0100

    Day 2 done

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,20 @@
 len("Hello")
+
+# Subscripting
+print("Hello"[0])
+
+# String
+print("123" + "345") # Concatenation
+
+# Integer = Whole number
+print(123 + 345)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Point Number
+3.14159
+
+# Boolean
+print(True)
+print(False)

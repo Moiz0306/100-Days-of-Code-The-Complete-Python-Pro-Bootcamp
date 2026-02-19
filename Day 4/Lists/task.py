2a44bb50e85d2c185dbe7c2b424f98a3dd8f7469
Moiz Wilayat <moiz.wilayat@student.howest.be>
2026-02-19 20:47:49 +0100

    Day 4 almost done

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,7 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey"]
+
+states_of_america[1] = "Pennsylvania"
+
+states_of_america.extend(["Angelaland", "Jack Bauer Land"])
+
+print(states_of_america)
